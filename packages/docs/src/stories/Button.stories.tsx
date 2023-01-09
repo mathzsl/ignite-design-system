d5b3099c28cad585fc46e3fc61b807f4ec2df995
matheuslima99 <maths.soares99@gmail.com>
2023-01-08 22:58:29 -0300

    Design System in React - Dark theme in storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Button } from '@matheussl-ignite-ui/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta
+
+export const Primary: StoryObj = {}
