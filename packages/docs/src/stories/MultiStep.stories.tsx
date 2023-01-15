ab6d4a8b88d237a67f7274903079c61455da16d8
matheuslima99 <maths.soares99@gmail.com>
2023-01-15 17:54:02 -0300

    Design System Components - Component: MultiStep

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,37 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import {
+  Box,
+  Text,
+  MultiStep,
+  MultiStepProps,
+} from '@matheussl-ignite-ui/react'
+
+export default {
+  title: 'Form/Multi Step',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 2,
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Observations</Text>
+          {Story()}
+        </Box>
+      )
+    },
+  ],
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4,
+  },
+}
