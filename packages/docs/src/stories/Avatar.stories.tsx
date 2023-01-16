594a8f3ab50dbf36a7ea526805e077152069d1fb
matheuslima99 <maths.soares99@gmail.com>
2023-01-16 01:02:53 -0300

    Storybook Improvements - Component controls

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -6,6 +6,14 @@ export default {
   component: Avatar,
   args: {
     src: 'https://github.com/matheuslima99.png',
+    alt: 'Matheus Lima',
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
   },
 } as Meta<AvatarProps>
 
--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -7,6 +7,34 @@ export default {
   component: Button,
   args: {
     children: 'Send',
+    variant: 'primary',
+    size: 'md',
+    disabled: false,
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+
+    variant: {
+      options: ['primary', 'secondary', 'tertiary'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+
+    onClick: {
+      action: 'Click',
+    },
   },
 } as Meta<ButtonProps>
 
