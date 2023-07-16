9aaa89747d8ca20f7f7f8ad9119cb575c501a7c2
matheuslima99 <maths.soares99@gmail.com>
2023-07-16 06:14:08 -0300

    feat(toast): AddToast component

---
+++ packages/docs/src/stories/Toast.stories.tsx
@@ -0,0 +1,39 @@
+import { Button, Toast, ToastProps } from '@matheussl-ignite-ui/react'
+import { Meta, StoryObj } from '@storybook/react'
+import { useEffect, useRef, useState } from 'react'
+
+const DemoToast = (props: ToastProps) => {
+  const [isOpen, setOpen] = useState(false)
+
+  const timeRef = useRef(0)
+
+  useEffect(() => {
+    return () => clearTimeout(timeRef.current)
+  }, [])
+
+  return (
+    <div>
+      <Button
+        onClick={() => {
+          setOpen(false)
+          window.clearTimeout(timeRef.current)
+          timeRef.current = window.setTimeout(() => setOpen(true), 100)
+        }}
+      >
+        Agendar
+      </Button>
+      <Toast open={isOpen} onOpenChange={setOpen} {...props} />
+    </div>
+  )
+}
+
+export default {
+  title: 'Surfaces/Toast',
+  component: DemoToast,
+  args: {
+    title: 'Agendamento realizado',
+    description: 'Quarta-feira, 23 de outubro às 16h',
+  },
+} as Meta<ToastProps>
+
+export const Primary: StoryObj<ToastProps> = {}
--- packages/react/src/components/Toast/index.tsx
+++ packages/react/src/components/Toast/index.tsx
@@ -1,5 +1,33 @@
-export interface ToastProps {}
+import { ToastProvider } from '@radix-ui/react-toast'
+import {
+  ToastClose,
+  ToastDescription,
+  ToastRoot,
+  ToastTitle,
+  ToastViewport,
+} from './styles'
+import { ComponentProps } from 'react'
+import { X } from 'phosphor-react'
 
-export function Toast() {}
+export interface ToastProps extends ComponentProps<typeof ToastRoot> {
+  title: string
+  description?: string
+}
+
+export function Toast({ title, description, ...props }: ToastProps) {
+  return (
+    <ToastProvider>
+      <ToastRoot {...props}>
+        <ToastTitle>{title}</ToastTitle>
+        {description && <ToastDescription>{description}</ToastDescription>}
+        <ToastClose asChild>
+          <X weight="light" size={20} />
+        </ToastClose>
+      </ToastRoot>
+
+      <ToastViewport />
+    </ToastProvider>
+  )
+}
 
 Toast.displayName = 'Toast'
